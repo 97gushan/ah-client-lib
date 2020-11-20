da86b1fc5fab97f747543cfe2b8356c3bf0a29aa
97gushan <97gustavh@gmail.com>
2020-11-20 11:00:17 +0100

    add certificate to http requests

--- utils/Http.cs
+++ utils/Http.cs
@@ -3,16 +3,48 @@ using System.Text;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ArrowHead.Utils
 {
     public class Http
     {
-        static readonly HttpClient client = new HttpClient();
+
+        static HttpClient client;
+
         private string baseURI;
 
-        public Http(string baseURI)
+        /// <summary>
+        /// Initialize a static HttpClient using the given certificate 
+        /// </summary>
+        /// <param name="certPath">Path to .p12 cert file</param>
+        /// <param name="verifyCert">Should the certificate be validated? 
+        /// if set to false then self-signed certs can be used</param>
+        static void initHttp(string certPath, bool verifyCert)
         {
+            HttpClientHandler handler = new HttpClientHandler();
+            X509Certificate certificate = new X509Certificate2(certPath, "123456");
+            handler.ClientCertificates.Add(certificate);
+
+            if (!verifyCert)
+            {
+                handler.ServerCertificateCustomValidationCallback =
+                    (httpRequestMessage, cert, cetChain, policyErrors) =>
+                {
+                    return true;
+                };
+            }
+            client = new HttpClient(handler);
+        }
+        public Http(string baseURI, string certPath, bool verifyCert)
+        {
+            // only init the http client once, as it is static it can be used by every instance
+            // without taking up more ports than needed
+            if (client == null)
+            {
+                initHttp(certPath, verifyCert);
+            }
+
             this.baseURI = baseURI;
         }
 
